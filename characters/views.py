4896e0d85e8510625c9abe97e3d571d248743ce6
Oleh Klivanets <oleg.klivanets@gmail.com>
2026-06-21 13:33:29 +0300

    improve character view

--- characters/views.py
+++ characters/views.py
@@ -1,7 +1,6 @@
 import random
 
 from django.db.models import QuerySet
-from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import status, generics
 from rest_framework.decorators import api_view
@@ -12,25 +11,17 @@ from characters.models import Character
 from characters.serializers import CharacterSerializer
 
 
-def get_random_character() -> Character:
-    pks = Character.objects.values_list("pk", flat=True)
-    random_pk = random.choice(pks)
-    return Character.objects.get(pk=random_pk)
-
-
-@extend_schema(
-    responses={
-        status.HTTP_200_OK: CharacterSerializer,
-    }
-)
+@extend_schema(responses={status.HTTP_200_OK: CharacterSerializer})
 @api_view(["GET"])
 def get_random_character_view(request: Request) -> Response:
     """Get a random character from the database."""
-    pks = Character.objects.values_list("pk", flat=True)
+    pks = list(Character.objects.values_list("pk", flat=True))
     if not pks:
-        return Response({"detail": "No characters found."}, status=status.HTTP_404_NOT_FOUND)
-    random_pk = random.choice(pks)
-    random_character = get_random_character()
+        return Response(
+            {"detail": "No characters found."},
+            status=status.HTTP_404_NOT_FOUND
+        )
+    random_character = Character.objects.get(pk=random.choice(pks))
     serializer = CharacterSerializer(random_character)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -40,13 +31,10 @@ class CharacterListView(generics.ListAPIView):
 
     def get_queryset(self) -> QuerySet:
         queryset = Character.objects.all()
-
         name = self.request.query_params.get("name")
         if name is not None:
             queryset = queryset.filter(name__icontains=name)
-
         return queryset
-
 
     @extend_schema(
         parameters=[
@@ -55,15 +43,9 @@ class CharacterListView(generics.ListAPIView):
                 description="Filter by name insensitive contains",
                 required=False,
                 type=str,
-
             )
         ]
     )
     def get(self, request, *args, **kwargs):
         """List characters with filter by name"""
         return super().get(request, *args, **kwargs)
-
-
-
-
-
